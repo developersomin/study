400234daa5bc66e04be71a29e56af32673fc96cc
developersomin <angiwon12345@nate.com>
2023-10-11 23:57:56 +0900

    Record back-end studies.

---
+++ login/server/src/api/user/user.resolver.ts
@@ -0,0 +1,21 @@
+import {Args, Int, Mutation, Resolver} from "@nestjs/graphql";
+import {UserService} from "./user.service";
+
+@Resolver()
+export class UserResolver{
+  constructor(
+      private readonly userService: UserService
+  ) {
+  }
+
+
+  @Mutation()
+  createUser(
+      @Args('email') email: string,
+      @Args('password') password: string,
+      @Args('name') name: string,
+      @Args({name: 'age', type: ()=>Int}) age: number
+  ){
+    return this.userService.create({email,password,name, age})
+  }
+}
---
+++ login/server/src/api/user/user.service.ts
@@ -0,0 +1,28 @@
+import {ConflictException, Injectable} from "@nestjs/common";
+import {Repository} from "typeorm";
+import {User} from "./entity/user.entity";
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UserService{
+    constructor(
+        private readonly userRepository: Repository<User>
+    ) {
+    }
+
+    findOneByEmail({email}){
+        return this.userRepository.findOne({where: {email}});
+    }
+
+    async create({email,password,name, age}){
+        const user = await this.findOneByEmail({email});
+        if (user) {
+            throw new ConflictException('이미 등록된 이메일입니다');
+        }
+        const hashPassword = await bcrypt.hash(password, 10);
+
+        return this.userRepository.save({email, password: hashPassword, name, age});
+    }
+
+}
+
